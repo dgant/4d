eb22b22f052f0612dd0bedcf52043312fb62ad5f
dgant <dgant@users.noreply.github.com>
2023-03-01 09:25:03 -0500

    Spinning in circles, mainly

--- 4d.js
+++ 4d.js
@@ -1,7 +1,7 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 import { PointerLockControls } from './node_modules/three/examples/jsm/controls/PointerLockControls.js';
 
-let camera, scene, renderer, controls, raycaster;
+let camera, scene, renderer, controls, protagMesh;
 
 const colliders = [];
 
@@ -11,44 +11,45 @@ let moveLeft = false;
 let moveRight = false;
 let canJump = false;
 let running = false;
+let jumping = false;
 let crouching = false;
 
 let prevMs = performance.now();
-const velocity = new THREE.Vector3();
-const direction = new THREE.Vector3();
+const protagV = new THREE.Vector3();
+const protagD = new THREE.Vector3();
 const vertex = new THREE.Vector3();
 const color = new THREE.Color();
 
 const gridSize = 20;
 const mapSize = 30;
-const protagDecel = 10.0;
-const protagAccel = 50.0;
-const protagJumpVelocity = 450.0;
-const protagSpeedCrouch = 150.0;
-const protagSpeedCrouchRun = 250.0;
-const protagSpeedWalk = 350.0; // Humans: 2.5-4mph
-const protagSpeedRun = 900.0; // Humans: 6mph jog, 18mph sprint
+const protagDecel = 10;
+const protagAccel = 50;
+const protagGravity = 1000;
+const protagJumpVelocity = 450;
+const protagSpeedCrouch = 150;
+const protagSpeedCrouchRun = 250;
+const protagSpeedWalk = 350; // Humans: 2.5-4mph
+const protagSpeedRun = 900; // Humans: 6mph jog, 18mph sprint
 const protagEyeLevel = 16.0;
+const protagRadius = 3;
 const protagHeight = gridSize;
 
 setup();
 loop();
 
 function setup() {
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.y = protagEyeLevel;
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xfff0f0);
   scene.fog = new THREE.Fog(0xfff0f0, 0, 400);
-
   const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
   light.position.set(0.5, 1, 0.75);
   scene.add(light);
 
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.y = protagEyeLevel;
   controls = new PointerLockControls(camera, document.body);
-  scene.add(controls.getObject());  
-
+  scene.add(controls.getObject());
   const onKeyDown = function (event) {
     controls.lock();
     switch (event.code) {
@@ -57,9 +58,13 @@ function setup() {
       case 'ArrowDown': case 'KeyS': moveBackward = true; break;
       case 'ArrowRight': case 'KeyD': moveRight = true; break;
       case 'ShiftLeft': case 'ShiftRight': running = true; break;
-      case 'ControlLeft': case 'ControlRight': crouching = true; break;
+      case 'ControlLeft': case 'ControlRight': crouching = ! jumping; break;
       case 'Space':
-        if (canJump === true) velocity.y = protagJumpVelocity;
+        if (canJump === true) {
+          protagV.y = protagJumpVelocity;
+          jumping = true;
+          crouching = false;
+        }
         canJump = false;
         break;
     }
@@ -77,13 +82,9 @@ function setup() {
   document.addEventListener('keydown', onKeyDown);
   document.addEventListener('keyup', onKeyUp);
 
-  raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, -1, 0), 0, protagEyeLevel);
-
-  // Floor
-
+  // Generate floor
   let floorGeometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
   floorGeometry.rotateX(- Math.PI / 2);
-
   let position = floorGeometry.attributes.position;
   for (let i = 0, l = position.count; i < l; ++i) {
     vertex.fromBufferAttribute(position, i);
@@ -92,21 +93,15 @@ function setup() {
     vertex.z += Math.random() * gridSize - 0.5 * gridSize;
     position.setXYZ(i, vertex.x, vertex.y, vertex.z);
   }
-
   floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
-
   position = floorGeometry.attributes.position;
   const colorsFloor = [];
-
   for (let i = 0, l = position.count; i < l; ++i) {
     color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
     colorsFloor.push(color.r, color.g, color.b);
   }
-
   floorGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsFloor, 3));
-
   const floorMaterial = new THREE.MeshBasicMaterial({ vertexColors: true });
-
   const floor = new THREE.Mesh(floorGeometry, floorMaterial);
   scene.add(floor);
  
@@ -114,7 +109,6 @@ function setup() {
   const boxGeometry = new THREE.BoxGeometry(gridSize, gridSize, gridSize).toNonIndexed();
   position = boxGeometry.attributes.position;
   const colorsBox = [];
-
   for (let i = 0, l = position.count; i < l; ++i) {
     color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
     colorsBox.push(color.r, color.g, color.b);
@@ -123,15 +117,19 @@ function setup() {
   for (let i = 0; i < 8000; ++i) {
     const boxMaterial = new THREE.MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: true });
     boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-
     const box = new THREE.Mesh(boxGeometry, boxMaterial);
     box.position.x = Math.floor(mapSize * (Math.random() * 2 - 1)) * gridSize;
     box.position.y = Math.floor(mapSize * (Math.random() * 2    )) * gridSize + gridSize / 2;
     box.position.z = Math.floor(mapSize * (Math.random() * 2 - 1)) * gridSize;
-
     scene.add(box);
     colliders.push(box);
   }
+
+  // Generate protag
+  const protagGeometry = new THREE.CylinderGeometry(protagRadius, protagRadius, protagHeight);
+  const protagMaterial = new THREE.MeshBasicMaterial({});
+  protagMesh = new THREE.Mesh(protagGeometry, protagMaterial);
+  protagMesh.translateY(protagHeight / 2);
 
   renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -150,47 +148,44 @@ function onWindowResize() {
 function loop() {
   requestAnimationFrame(loop);
 
-  const nowMs = performance.now();
+  const nowMs = performance.now();  
+  const deltaMs = (nowMs - prevMs) / 1000;
 
   if (controls.isLocked === true) {
-    raycaster.ray.origin.copy(controls.getObject().position);
-    raycaster.ray.origin.y -= protagEyeLevel;
-
-    const intersections = raycaster.intersectObjects(colliders, false);
-    const onObject = intersections.length > 0;
-    for (const intersector in intersections) {
-      console.log(intersector.toString());
+    let onObject = false;
+    for (const collider of colliders) {
+      // This treats protag as square
+      if (protagMesh.position.manhattanDistanceTo(collider.position) <= gridSize + protagRadius) {
+        onObject = true;
+      }
     }
 
-    const deltaMs = (nowMs - prevMs) / 1000;
-
-    // Deceleration
-    velocity.x -= velocity.x * protagDecel * deltaMs;
-    velocity.z -= velocity.z * protagDecel * deltaMs;
-    velocity.y -= 9.8 * 100.0 * deltaMs; // 100.0 = mass
-
-    direction.z = Number(moveForward) - Number(moveBackward);
-    direction.x = Number(moveRight) - Number(moveLeft);
-    direction.normalize(); // this ensures consistent movements in all directions
+    // Movement
+    protagV.x -= protagV.x * protagDecel * deltaMs;
+    protagV.z -= protagV.z * protagDecel * deltaMs;
+    protagV.y -= protagGravity * deltaMs;
+    protagD.z = Number(moveForward) - Number(moveBackward);
+    protagD.x = Number(moveRight) - Number(moveLeft);
+    protagD.normalize(); // this ensures consistent movements in all protagDs
 
     const deltaV = running ? protagSpeedRun : protagSpeedWalk;
-    if (moveForward || moveBackward) velocity.z -= direction.z * deltaV * deltaMs;
-    if (moveLeft || moveRight) velocity.x -= direction.x * deltaV * deltaMs;
+    if (moveLeft    || moveRight)    protagV.x -= protagD.x * deltaV * deltaMs;
+    if (moveForward || moveBackward) protagV.z -= protagD.z * deltaV * deltaMs;
 
     if (onObject === true) {
-      velocity.y = Math.max(0, velocity.y);
+      protagV.y = Math.max(0, protagV.y);
       canJump = true;
     }
 
-    controls.moveRight(- velocity.x * deltaMs);
-    controls.moveForward(- velocity.z * deltaMs);
-    controls.getObject().position.y += (velocity.y * deltaMs); 
-
+    controls.moveRight(- protagV.x * deltaMs);
+    controls.moveForward(- protagV.z * deltaMs);
+    controls.getObject().position.y += (protagV.y * deltaMs);
     if (controls.getObject().position.y < protagEyeLevel) {
-      velocity.y = 0;
+      protagV.y = 0;
       controls.getObject().position.y = protagEyeLevel;
       canJump = true;
     }
+    protagMesh.position.set(controls.getObject().position);
   }
   prevMs = nowMs;
   renderer.render(scene, camera);
