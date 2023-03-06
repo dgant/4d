8d8b2cb8fe5fd322310a539ae77f222896a0c009
dgant <dgant@users.noreply.github.com>
2023-03-06 17:34:05 -0500

    Exporting FPSControls

--- 4d.js
+++ 4d.js
@@ -1,12 +1,14 @@
 import * as THREE from 'three';
 import { MeshBVH, MeshBVHVisualizer, StaticGeometryGenerator } from '/node_modules/three-mesh-bvh/build/index.module.js';
 
+import FPSControls from './fpscontrols.js';
+
 // Units:
 // * 1 distance unit = 1 meter
 // * 1 unit of time = 1 ms
+// Treat all constants as eg. meters, seconds, meters per second (per second), etc.
 const gridSize = 2;
 const mapSize = 12;
-
 // Height/eye level taken as gender-averaged from
 // https://www.firstinarchitecture.co.uk/average-male-and-female-dimensions/
 const playerHeight = 1.675;
@@ -19,7 +21,7 @@ const playerTopSpeedRun = 8; // Humans: 8m/s sprint, 3m/s jog, 1.8 m/s walk
 const playerTopSpeedWalkMultiplier = 0.4;
 const playerTopSpeedTotal = 64; // Top speed including falling; restricted to allow setting physicsMaxStep
 const playerStrafeMultiplier = 0.65;
-const playerDecelTime = 0.6;
+const playerDecelTime = 0.4;
 const playerAccelTime = 1.0;
 const playerDecel = playerTopSpeedRun / playerDecelTime;
 const playerAccel = playerTopSpeedRun / playerAccelTime + playerDecel;
@@ -41,68 +43,6 @@ const playerControlV3 = new THREE.Vector3(0, 0, 0);
 const playerDecelV3 = new THREE.Vector3(0, 0, 0);
 const playerAccelV3 = new THREE.Vector3(0, 0, 0);
 const color = new THREE.Color();
-
-// Based on THREE.PointerLockControls
-class FPSControls extends THREE.EventDispatcher {
-	constructor(camera, domElement) {
-    const _euler = new THREE.Euler(0, 0, 0, 'YXZ');
-    const _changeEvent = { type: 'change' };
-    const _lockEvent = { type: 'lock' };
-    const _unlockEvent = { type: 'unlock' };
-    const _HALF_PI = Math.PI / 2;
-		super();    
-		this.domElement = domElement;
-		this.isLocked = false;
-		this.minPolarAngle = 0; // radians
-		this.maxPolarAngle = Math.PI; // radians
-		this.pointerSpeed = 1.0;
-		const scope = this;
-		function onMouseMove(event) {
-			if (scope.isLocked === false) return;
-			const movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-			const movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-			_euler.setFromQuaternion(camera.quaternion);
-			_euler.y -= movementX * 0.002 * scope.pointerSpeed;
-			_euler.x -= movementY * 0.002 * scope.pointerSpeed;
-			_euler.x = Math.max(_HALF_PI - scope.maxPolarAngle, Math.min(_HALF_PI - scope.minPolarAngle, _euler.x));
-			camera.quaternion.setFromEuler(_euler);
-			scope.dispatchEvent(_changeEvent);
-		}
-		function onPointerlockChange() {
-      scope.isLocked = scope.domElement.ownerDocument.pointerLockElement === scope.domElement;
-			scope.dispatchEvent(scope.isLocked ? _lockEvent : _unlockEvent);
-		}
-		function onPointerlockError() { console.error('FPSControls: Unable to use Pointer Lock API'); }
-		this.connect = function () {
-			scope.domElement.ownerDocument.addEventListener('mousemove', onMouseMove);
-			scope.domElement.ownerDocument.addEventListener('pointerlockchange', onPointerlockChange);
-			scope.domElement.ownerDocument.addEventListener('pointerlockerror', onPointerlockError);
-		};
-		this.disconnect = function() {
-			scope.domElement.ownerDocument.removeEventListener('mousemove', onMouseMove);
-			scope.domElement.ownerDocument.removeEventListener('pointerlockchange', onPointerlockChange);
-			scope.domElement.ownerDocument.removeEventListener('pointerlockerror', onPointerlockError);
-		};
-		this.dispose = function() { this.disconnect(); };
-		this.getDirection = function() {
-			const direction = new THREE.Vector3(0, 0, - 1);
-			return function(v) { return v.copy(direction).applyQuaternion(camera.quaternion); };
-		}();
-    this.getAzimuthalDirection = function(v) {
-			const output = this.getDirection(v);
-      output.y = 0;
-      output.normalize();
-      return output;
-		}
-		this.lock = function() {
-      scope.domElement.requestPointerLock();
-    }
-		this.unlock = function() {
-      scope.domElement.ownerDocument.exitPointerLock;
-    }
-		this.connect();
-	}
-}
 
 function setup() {
   // Generate camera
